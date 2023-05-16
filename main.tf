52e7d230bdb9c95edce37fb2301a4f7ad6ca2841
Clare Tucker <c.tucker@cmcmarkets.com>
2023-05-16 10:49:43 +0100

    ahh

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id              = module.dev.public_subnets[0]
+  vpc_security_group_ids = [module.dev.security_group_id]
 
   tags = {
     Name = "HelloWorld"
@@ -51,8 +51,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  vpc_id             = module.dev.vpc_id
+  subnets            = module.dev.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
  
 
@@ -91,7 +91,7 @@ module "blog_sg" {
   version = "4.17.2"
 
   name= "blog"
-  vpc_id = module.blog_vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
